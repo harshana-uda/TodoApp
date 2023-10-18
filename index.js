f4086cddd365c3dde6dd67d236529fecb3e8df1d
Harshana Udayanga <sudayanga1820@gmail.com>
2023-10-18 21:17:25 +0530

    Added basic HTML, CSS, and JavaScript code for displaying and adding tasks to the ToDo list

---
+++ index.js
@@ -0,0 +1,29 @@
+const taskInput = document.getElementById('task-input');
+const taskList = document.getElementById('list')
+const alertElement = document.getElementById('alert');
+
+const date = new Date().toLocaleDateString(undefined, {month: 'long', day: '2-digit'})
+
+document.getElementById('heading').innerText = "Today's " + date
+
+function insertTask(content) {
+    const taskContainer = document.createElement('div');
+    taskContainer.className = 'task'
+    taskContainer.insertAdjacentHTML(`afterbegin`, `
+        <p>${content}</p>
+    `)
+    
+    taskList.appendChild(taskContainer)
+}
+
+
+document.getElementById('btn-add-task').addEventListener('click', ()=> {
+    if (alertElement) {
+        alertElement.remove();
+    }
+
+
+    insertTask(taskInput.value)
+    // Clear the input's value 
+    taskInput.value = "";
+})
